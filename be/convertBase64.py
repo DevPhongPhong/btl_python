cd0968ede0eed0a065a380184009e1f7e1559fe8
DESKTOP-BNHUTNH\phong <phongnh2405@gmail.com>
2022-12-01 21:02:18 +0700

    commit

--- be/convertBase64.py
+++
@@ -1,8 +0,0 @@
-import base64
-def convertToImg(base64code,dataFileName):
-    imgdata = base64.b64decode(base64code)
-    filename = 'static/img/'+dataFileName 
-    with open(filename, 'wb') as f:
-        f.write(imgdata)
-    return '/'+filename
-
